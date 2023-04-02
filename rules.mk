6d7c4ad2ec54e97455466eab78f28ea142eb2730
mejobloggs <mejobloggs@gmail.com>
2023-04-02 16:34:42 +1200

    first commit

---
+++ rules.mk
@@ -0,0 +1,9 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = no
+LTO_ENABLE      = yes
+
+RGBLIGHT_SUPPORTED = no
+RGB_MATRIX_SUPPORTED = no
+
+#test
